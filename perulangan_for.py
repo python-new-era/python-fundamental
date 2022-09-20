db58f39b14dc695087b56275cf41112bd53a6cde
saasin <hdiudhusd@aol.com>
2022-09-20 20:20:57 +0700

    looping for dan while

---
+++ perulangan_for.py
@@ -0,0 +1,10 @@
+jumlah_buku = 10
+print("Ibu Berkata, :Baca Semua Bukumu")
+
+jumlah_buku_yang_sudah_dibaca = 0
+
+
+for jumlah_buku_yang_sudah_dibaca in range(jumlah_buku):
+    print(f'Jumlah Buku yang sudah di baca adalah {jumlah_buku_yang_sudah_dibaca}')
+
+print(jumlah_buku_yang_sudah_dibaca)
---
+++ perulangan_while.py
@@ -0,0 +1,8 @@
+jumlah_buku = 10
+print("Ibu berkata , Baca Semua Buku mu")
+
+jumlah_buku_yang_sudah_dibaca = 0
+
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
+    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
+    print(f'Buke ke {jumlah_buku_yang_sudah_dibaca} yang sudah di baca')
