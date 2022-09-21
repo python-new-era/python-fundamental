6422c3f0b239b79a99e715c76bb3f09dfa09ebe0
saasin <hdiudhusd@aol.com>
2022-09-21 12:52:09 +0700

    nested dictionary

--- function_sekolah_koding.py
+++ function_sekolah_koding.py
@@ -29,3 +29,48 @@ hasil = cal(10,40)
 
 print(hasil + 20)
 
+
+print("Keyword argument")
+
+def printData(nam,hoby):
+    print(f"Nama : {nam} Hoby : {hoby}")
+
+
+printData(nam ="joni" ,hoby='nyanyi')
+
+
+print('*agrs dan **Kwargs')
+
+def cobaprint(*args):
+    for jeneng in args:
+        print(jeneng)
+
+cobaprint('Joni' ,'Ronan')
+
+
+print('*agrs dan **Kwargs')
+
+def cobaprint2(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key} : {value}")
+
+cobaprint2(nama ="joni",hoby="berenang")
+
+
+def cobaArgs(*args):
+    for name in args:
+        print(name)
+
+cobaArgs("doiana",'rumain')
+
+print("\nCoba Kwargs")
+
+def cobaKwargs(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key} - {value}")
+
+cobaKwargs(nama ="joni",alamat="metro",pekerjaan="desainer")
+
+
+
+
